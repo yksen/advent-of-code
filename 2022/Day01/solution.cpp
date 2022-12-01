2f663117c43f870f22d841419f985baec589d03a
xen <selegakamil@gmail.com>
2022-12-01 19:26:58 +0100

    Solve part one of Day 1

--- 2022/Day01/solution.cpp
+++ 2022/Day01/solution.cpp
@@ -1,4 +1,23 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
 int main()
 {
-    return 0;
+    std::string filename = "input.txt";
+    std::ifstream file(filename);
+    if (!file.is_open())
+        return 1;
+
+    uint64_t sum = 0, highestSum = 0;
+    for (std::string line; std::getline(file, line);)
+        if (line != "")
+            sum += std::stoull(line);
+        else
+        {
+            highestSum = std::max(highestSum, sum);
+            sum = 0;
+        }
+
+    std::cout << highestSum << std::endl;
 }
