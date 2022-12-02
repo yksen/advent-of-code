a2f16d0bf57b121709c8547779c3617eac2ae186
xen <selegakamil@gmail.com>
2022-12-02 18:41:52 +0100

    Solve part two and refactor the code

--- 2022/Day02/solution.cpp
+++ 2022/Day02/solution.cpp
@@ -18,10 +18,10 @@ std::map<char, uint64_t> shapeValues = {
     {'Y', 2},
     {'Z', 3}};
 
-std::map<char, char> winningCombinations = {
-    {'C', 'X'},
-    {'A', 'Y'},
-    {'B', 'Z'}};
+std::map<char, Result> expectedResults = {
+    {'X', Result::LOSS},
+    {'Y', Result::DRAW},
+    {'Z', Result::WIN}};
 
 std::vector<std::pair<char, char>> parseStrategyGuideFromFile(std::string filename)
 {
@@ -32,37 +32,64 @@ std::vector<std::pair<char, char>> parseStrategyGuideFromFile(std::string filena
     std::vector<std::pair<char, char>> strategyGuide;
     while (!file.eof())
     {
-        std::pair<char, char> playersDecisions;
-        file >> playersDecisions.first >> playersDecisions.second;
-        if (playersDecisions.first == 0 || playersDecisions.second == 0)
+        std::pair<char, char> roundInputs;
+        file >> roundInputs.first >> roundInputs.second;
+        if (roundInputs.first == 0 || roundInputs.second == 0)
             break;
-        strategyGuide.push_back(playersDecisions);
+        strategyGuide.push_back(roundInputs);
     }
     return strategyGuide;
 }
 
-Result calculateResult(std::pair<char, char> decisions)
+Result getResult(std::pair<char, char> roundInputs)
 {
-    for (std::pair<char, char> winningCombination : winningCombinations)
-        if (decisions == winningCombination)
-            return Result::WIN;
-        else if (shapeValues.at(decisions.first) == shapeValues.at(decisions.second))
-            return Result::DRAW;
-    return Result::LOSS;
+    char opponentDecision = roundInputs.first;
+    char ownDecision = roundInputs.second;
+    if (shapeValues.at(opponentDecision) == shapeValues.at(ownDecision))
+        return Result::DRAW;
+    else if (shapeValues.at(opponentDecision) % 3 + 1 == shapeValues.at(ownDecision))
+        return Result::WIN;
+    else
+        return Result::LOSS;
 }
 
-uint64_t calculateTotalScore(std::vector<std::pair<char, char>> strategyGuide)
+char deduceOwnDecision(char opponentDecision, Result expectedResult)
+{
+    if (expectedResult == Result::DRAW)
+        return opponentDecision;
+    else if (expectedResult == Result::LOSS)
+        return (opponentDecision % 3) + 'A';
+    else
+        return (opponentDecision + 2) % 3 + 'A';
+}
+
+uint64_t partOne(std::vector<std::pair<char, char>> strategyGuide)
 {
     uint64_t score = 0;
     for (auto playersDecisions : strategyGuide)
-        score += uint64_t(calculateResult(playersDecisions)) + shapeValues[playersDecisions.second];
+        score += uint64_t(getResult(playersDecisions)) + shapeValues.at(playersDecisions.second);
+    return score;
+}
+
+uint64_t partTwo(std::vector<std::pair<char, char>> strategyGuide)
+{
+    uint64_t score = 0;
+    for (auto roundInputs : strategyGuide)
+    {
+        char opponentDecision = roundInputs.first;
+        Result expectedResult = expectedResults.at(roundInputs.second);
+        char ownDecision = deduceOwnDecision(opponentDecision, expectedResult);
+        score += uint64_t(expectedResult) + shapeValues.at(ownDecision);
+    }
     return score;
 }
 
 int main()
 {
     auto strategyGuide = parseStrategyGuideFromFile("input.txt");
-    std::cout << calculateTotalScore(strategyGuide) << std::endl;
+
+    std::cout << partOne(strategyGuide) << std::endl;
+    std::cout << partTwo(strategyGuide) << std::endl;
 
     return 0;
 }
