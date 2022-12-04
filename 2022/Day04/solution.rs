840c154b609f2a1bc0a0d0e3467aba5df9216b75
xen <selegakamil@gmail.com>
2022-12-04 14:57:21 +0100

    Add part two context and solution

--- 2022/Day04/solution.rs
+++ 2022/Day04/solution.rs
@@ -1,12 +1,13 @@
 use std::{fs::File, io::BufRead};
 
 fn main() {
-    let input_path: String = "input.txt".to_string();
+    let input_path = "input.txt";
 
     part_one(input_path);
+    part_two(input_path);
 }
 
-fn part_one(input_path: String) {
+fn part_one(input_path: &str) {
     let file = File::open(input_path).unwrap();
     let reader = std::io::BufReader::new(file);
 
@@ -20,6 +21,24 @@ fn part_one(input_path: String) {
         if first_elf[0] <= second_elf[0] && first_elf[1] >= second_elf[1]
             || second_elf[0] <= first_elf[0] && second_elf[1] >= first_elf[1]
         {
+            total += 1;
+        }
+    }
+    println!("{}", total);
+}
+
+fn part_two(input_path: &str) {
+    let file = File::open(input_path).unwrap();
+    let reader = std::io::BufReader::new(file);
+
+    let mut total = 0;
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let elves: Vec<&str> = line.split(",").collect();
+        let first_elf: Vec<u64> = elves[0].split("-").map(|x| x.parse().unwrap()).collect();
+        let second_elf: Vec<u64> = elves[1].split("-").map(|x| x.parse().unwrap()).collect();
+
+        if first_elf[1] >= second_elf[0] && first_elf[0] <= second_elf[1] {
             total += 1;
         }
     }
