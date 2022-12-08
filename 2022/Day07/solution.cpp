09556465b7a6bca47bb18e8d13f656ba9bdec972
xen <selegakamil@gmail.com>
2022-12-08 20:44:21 +0100

    Add Day 7 part two solution

--- 2022/Day07/solution.cpp
+++ 2022/Day07/solution.cpp
@@ -4,7 +4,11 @@
 #include <sstream>
 #include <vector>
 
-const uint64_t MAX_TOTAL_DIRECTORY_SIZE = 1'000'00;
+const uint64_t MAX_TOTAL_DIRECTORY_SIZE = 100'000;
+const uint64_t TOTAL_DISK_SPACE = 70'000'000;
+const uint64_t MIN_UNUSED_SPACE = 30'000'000;
+
+static uint64_t smallest_candidate_directory_size;
 
 std::vector<std::string> splitString(const std::string &str, const char &delimiter)
 {
@@ -25,10 +29,9 @@ std::vector<std::string> readLinesFromFile(const std::string &file_path)
     return lines;
 }
 
-uint64_t searchDepthAndSumSizes(const std::vector<std::string> &lines, size_t &i, uint64_t &total_size)
+uint64_t searchDepthAndSumSizes(const std::vector<std::string> &lines, size_t &i, uint64_t &total_size, uint64_t space_needed)
 {
     uint64_t directory_size = 0;
-
     while (i < lines.size() - 1)
     {
         ++i;
@@ -43,31 +46,52 @@ uint64_t searchDepthAndSumSizes(const std::vector<std::string> &lines, size_t &i
                 continue;
             else if (chunks[2] == "..")
             {
-                if (directory_size < MAX_TOTAL_DIRECTORY_SIZE)
+                if (directory_size <= MAX_TOTAL_DIRECTORY_SIZE)
                     total_size += directory_size;
+                if (directory_size >= space_needed && directory_size < smallest_candidate_directory_size)
+                    smallest_candidate_directory_size = directory_size;
                 return directory_size;
             }
             else
-                directory_size += searchDepthAndSumSizes(lines, i, total_size);
+                directory_size += searchDepthAndSumSizes(lines, i, total_size, space_needed);
         }
     }
 
     if (directory_size < MAX_TOTAL_DIRECTORY_SIZE)
         total_size += directory_size;
+    if (directory_size >= space_needed && directory_size < smallest_candidate_directory_size)
+        smallest_candidate_directory_size = directory_size;
     return directory_size;
 }
 
-uint64_t partOne(const std::vector<std::string> &lines)
+std::pair<uint64_t, uint64_t> partOne(const std::vector<std::string> &lines)
 {
     size_t index = 0;
-    uint64_t total_size = 0;
-    searchDepthAndSumSizes(lines, index, total_size);
-    return total_size;
+    uint64_t sum_of_directory_sizes = 0;
+    uint64_t total_size = searchDepthAndSumSizes(lines, index, sum_of_directory_sizes, 0);
+
+    return {sum_of_directory_sizes, total_size};
+}
+
+uint64_t partTwo(const std::vector<std::string> &lines, uint64_t total_size)
+{
+    size_t index = 0;
+    uint64_t sum_of_directory_sizes = 0;
+    uint64_t space_needed = MIN_UNUSED_SPACE - (TOTAL_DISK_SPACE - total_size);
+
+    smallest_candidate_directory_size = MIN_UNUSED_SPACE;
+    searchDepthAndSumSizes(lines, index, sum_of_directory_sizes, space_needed);
+
+    return smallest_candidate_directory_size;
 }
 
 int main()
 {
     auto lines = readLinesFromFile("input.txt");
 
-    std::cout << partOne(lines) << std::endl;
+    auto [sum_of_directory_sizes, total_size] = partOne(lines);
+    auto smallest_directory_size = partTwo(lines, total_size);
+
+    std::cout << sum_of_directory_sizes << std::endl;
+    std::cout << smallest_directory_size << std::endl;
 }
