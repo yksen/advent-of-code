190ab1214cefa11d7e3e7008ec850d9c8ed0932e
xen <selegakamil@gmail.com>
2024-12-18 19:06:33 +0100

    2024-17-2

--- 2024/src/17.cpp
+++ 2024/src/17.cpp
@@ -1,77 +1,83 @@
 #include <aoc/input.hpp>
 #include <aoc/utils.hpp>
-#include <cmath>
+#include <iterator>
+#include <optional>
 #include <print>
+#include <span>
 #include <string>
 #include <vector>
 
+std::optional<int64_t> run(const std::span<int> program, int &i, std::map<int, int64_t> &combo,
+                           bool ignore_adv = false) {
+  const auto opcode{program[i]}, operand{program[i + 1]};
+  switch (opcode) {
+    case 0:  // adv
+      if (!ignore_adv) combo[4] = combo[4] >> combo[operand];
+      break;
+    case 1:  // bxl
+      combo[5] ^= operand;
+      break;
+    case 2:  // bst
+      combo[5] = combo[operand] % 8;
+      break;
+    case 3:  // jnz
+      if (combo[4] != 0) i = operand - 2;
+      break;
+    case 4:  // bxc
+      combo[5] ^= combo[6];
+      break;
+    case 5:  // out
+      return combo[operand] % 8;
+    case 6:  // bdv
+      combo[5] = combo[4] >> combo[operand];
+      break;
+    case 7:  // cdv
+      combo[6] = combo[4] >> combo[operand];
+      break;
+  }
+  return {};
+}
+
+std::optional<int64_t> find(const std::vector<int> &program, std::span<int> target,
+                            std::map<int, int64_t> combo, int64_t result) {
+  if (target.empty()) return result;
+  for (int i{0}; i < 8; ++i) {
+    combo[4] = result << 3 | i;
+    combo[5] = 0;
+    combo[6] = 0;
+    for (int j{0}; j < program.size() - 2; j += 2) {
+      auto output{run(target, j, combo, true)};
+      if (output && *output == target.back()) {
+        auto res{find(program, std::span{target.begin(), std::prev(target.end())}, combo, combo[4])};
+        if (!res) continue;
+        return *res;
+      }
+    }
+  }
+  return {};
+}
+
 int main(int argc, char **argv) {
   auto input{aoc::fetch_input(argc, argv)};
-  std::vector<int64_t> registers, program;
+  std::vector<int> program, output;
+  std::map<int, int64_t> combo{{1, 1}, {2, 2}, {3, 3}};
   bool parse_registers{true};
-  for (auto line : input) {
-    if (line.empty()) {
+  for (auto line : input)
+    if (line.empty())
       parse_registers = false;
-      continue;
-    }
-    if (parse_registers)
-      registers.emplace_back(aoc::get_numbers(line).front());
+    else if (parse_registers)
+      combo[combo.size() + 1] = aoc::get_numbers(line).front();
     else
       for (auto n : aoc::get_numbers(line)) program.emplace_back(n);
-  }
-
-  std::map<int, int64_t *> combo{{1, new int64_t{1}}, {2, new int64_t{2}}, {3, new int64_t{3}},
-                                 {4, &registers[0]},  {5, &registers[1]},  {6, &registers[2]}};
-  int64_t reg_init{-1};
-  std::string output{};
 
   auto join_vector{[](auto vec) {
     std::string res{};
     for (auto v : vec) res += std::to_string(v) + ',';
-    return res;
+    return res.substr(0, res.size() - 1);
   }};
-  const std::string program_str{join_vector(program)};
 
-  bool skip{false};
-  while (output != program_str) {
-    ++reg_init;
-    output = "";
-    registers = {reg_init, 0, 0};
-    skip = false;
-    for (int i{0}; i < program.size(); i += 2) {
-      const auto opcode{program[i]}, operand{program[i + 1]};
-      switch (opcode) {
-        case 0:  // adv
-          registers[0] = registers[0] / (1 << *combo[operand]);
-          break;
-        case 1:  // bxl
-          registers[1] ^= operand;
-          break;
-        case 2:  // bst
-          registers[1] = *combo[operand] % 8;
-          break;
-        case 3:  // jnz
-          if (registers[0] != 0) i = operand - 2;
-          break;
-        case 4:  // bxc
-          registers[1] ^= registers[2];
-          break;
-        case 5:  // out
-          output += std::to_string(*combo[operand] % 8) + ',';
-          if (output.size() > program_str.size() ||
-              output[output.size() - 2] != program_str[output.size() - 2])
-            skip = true;
-          break;
-        case 6:  // bdv
-          registers[1] = registers[0] / (1 << *combo[operand]);
-          break;
-        case 7:  // cdv
-          registers[2] = registers[0] / (1 << *combo[operand]);
-          break;
-      }
-      if (skip) break;
-    }
-  }
-
-  std::println("{}", reg_init);
+  auto c{combo};
+  for (int i{0}; i < program.size(); i += 2)
+    if (auto res{run(program, i, combo)}) output.emplace_back(*res);
+  std::println("{} {}", join_vector(output), *find(program, program, c, 0));
 }
